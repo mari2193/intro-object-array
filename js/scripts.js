2b3a3d05d550b5198e5853e7120c3216aa6b677d
Mari2102 <59822207+Mari2102@users.noreply.github.com>
2020-01-27 16:54:18 +0200

    Added some details

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ window.dataLayer.forEach(function(event) {
   var newEvent = document.createElement("li");
   newEvent.innerHTML =
     '<div class="info"><h2 class="title">' +
-    event.short_description.fi +
     event.name.fi +
-    '</h2><p class = "desc">' +
+    '</h2> <p class = "desc">' +
     event.description.fi +
-    event.info_url.fi +
-    +"</p></div>";
+    "</p></div>";
+  event.info_url.fi;
   document.getElementById("event-list").appendChild(newEvent);
 });
